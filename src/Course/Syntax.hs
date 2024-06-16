d6feb938aa9869ccbdfb97fbec2cab592fa179b3
Luke Taranto <luke.taranto@gmail.com>
2024-06-16 21:41:47 +1000

    update Syntax

--- src/Course/Syntax.hs
+++ src/Course/Syntax.hs
@@ -1,40 +1,26 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Course.Syntax where
 
-import Course.Core ( Eq, Fractional((/)), Num((*)), Show )
-import Prelude (Double, Integer, Rational, pi, Int, fromInteger)
+import Course.Core
+import Prelude (Double)
 
 -- Write a constant approximating Pi
--- pi :: Double
--- pi = 3.418
+pi :: Double
+pi = 3.418
 
 -- Write a data type called 'Shape' that can hold either Circles, Rectangles, or Triangles
 data Shape = Circle Double | Rectangle Double Double | Triangle Double Double
-  deriving (Eq, Show)
 
--- Write a function that takes a Shape and calculates the area of that shape
+-- Write a function that takes a Shape, and calculates the area of that shape
 area :: Shape -> Double
 area shape = case shape of
   Circle r -> pi * r * r
   Rectangle w h -> w * h
-  Triangle b h -> b * h / 2
+  Triangle b h -> 0.5 * b * h
 
---area = error "todo: Course.Synatx#area"
--- area (Circle r) = pi * r * r
--- area (Rectangle w h) = w * h
--- area (Triangle a b c) = sqrt (s * (s - a) * (s - b) * (s - c))
---   where s = (a + b + c) / 2
--- area (Triangle a b c) =
---   let s = (a + b + c) / 2
---   in sqrt (s * (s - a) * (s - b) * (s - c))
-
-
--- Define a Circle with radius 5
--- myCircle :: Shape
--- myCircle = Circle 5
-
--- Call the area function with the Circle
--- result :: Double
--- result = area myCircle
-
+-- Alternatively, we can use pattern matching in the function definition
+area' :: Shape -> Double
+area' (Circle r) = pi * r * r
+area' (Rectangle w h) = w * h
+area' (Triangle b h) = 0.5 * b * h
 
